187147518d3f49e1d0aec4278f9f863f123cd400
Lumbar10 <luigi.cusanno@studenti.unicam.it>
2026-07-27 14:22:46 +0200

    Aggiunta barra di ricerca

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProdottoService } from './services/prodotto';
-import { Observable, BehaviorSubject, switchMap } from 'rxjs';
+import { Observable, BehaviorSubject, switchMap, map, debounceTime, combineLatest, distinctUntilChanged } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -19,13 +19,22 @@ export class App {
     quantita: 0
   };
 
+  termineRicerca = '';
+  private searchSubject = new BehaviorSubject<string>('');
+
   prodottoInModifica: any = null;
 
   private refresh$ = new BehaviorSubject<void>(undefined);
 
   constructor(private prodottoService: ProdottoService) {
-    this.prodotti$ = this.refresh$.pipe(
-      switchMap(() => this.prodottoService.getProdotti())
+    this.prodotti$ = combineLatest([
+      this.refresh$,
+      this.searchSubject.pipe(debounceTime(300), distinctUntilChanged())
+    ]).pipe(
+      switchMap(() => this.prodottoService.getProdotti()),
+      map(prodotti => prodotti.filter(p =>
+        p.nome.toLowerCase().includes(this.termineRicerca.toLowerCase())
+      ))
     );
   }
 
@@ -52,4 +61,10 @@ export class App {
       });
     }
   }
+
+  cercaProdotti(termine: string) {
+    this.searchSubject.next(termine);
+  }
+
+
 }
